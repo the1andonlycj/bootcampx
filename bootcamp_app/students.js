57356939f6e2669d43a24eb666181c9366a45bb7
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-06-25 16:11:23 +0000

    Added JS queries for a few data points.

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -8,14 +8,14 @@ const pool = new Pool({
 });
 
 pool.query(`
-SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-LIMIT 5;
+WHERE cohorts.name LIKE '%${process.argv[2]}%'
+LIMIT ${process.argv[3] || 5};
 `)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
     })
-  })
-  .catch(err => console.error('query error', err.stack));
+  }).catch(err => console.error('query error', err.stack));
