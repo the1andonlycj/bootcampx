dbf08c66291cb7ce6d6fccb4d0cc97ca360936d5
Charlie Hayes <cjhayestutor@gmail.comgit config --global user.email cjhayestutor@gmail.com>
2020-06-25 00:48:45 +0000

    Added JS support.

---
+++ bootcamp_app/students.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+LIMIT 5;
+`)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    })
+  })
+  .catch(err => console.error('query error', err.stack));
